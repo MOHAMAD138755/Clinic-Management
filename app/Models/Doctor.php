6d31a75c33ac406d4183eb2a72f6aecc7d0945f4
mohamadtorki <mohamadprograming123@gmail.com>
2026-08-28 19:29:48 +0330

    create doctor and specilty model and migration

---
+++ app/Models/Doctor.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Doctor extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'id',
+        'first_name',
+        'last_name',
+        'medical_system_number',
+        'phone',
+        'active',
+        'biography',
+        'specialty_id'
+    ];
+
+    public function specialty()
+    {
+        return $this->belongsTo(Specialty::class);
+    }
+}
