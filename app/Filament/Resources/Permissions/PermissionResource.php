c1d1a2a01bbe584627b0055a4191972880d1ae46
mohamadtorki <mohamadprograming123@gmail.com>
2026-08-31 13:23:59 +0330

    create Roles And Permissions to panel

---
+++ app/Filament/Resources/Permissions/PermissionResource.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Filament\Resources\Permissions;
+
+use App\Filament\Resources\Permissions\Pages\CreatePermission;
+use App\Filament\Resources\Permissions\Pages\EditPermission;
+use App\Filament\Resources\Permissions\Pages\ListPermissions;
+use App\Filament\Resources\Permissions\Schemas\PermissionForm;
+use App\Filament\Resources\Permissions\Tables\PermissionsTable;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Spatie\Permission\Models\Permission;
+
+class PermissionResource extends Resource
+{
+    protected static ?string $model = Permission::class;
+
+    protected static string|BackedEnum|null $navigationIcon = 'heroicon-o-lock-closed';
+
+    protected static ?string $recordTitleAttribute = 'Permission Management';
+
+    protected static string|null|\UnitEnum $navigationGroup = 'Role And Permissions';
+
+    public static function form(Schema $schema): Schema
+    {
+        return PermissionForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return PermissionsTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListPermissions::route('/'),
+            'create' => CreatePermission::route('/create'),
+            'edit' => EditPermission::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getRecordRouteBindingEloquentQuery(): Builder
+    {
+        return parent::getRecordRouteBindingEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Roles/Tables/RolesTable.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Filament\Resources\Roles\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ForceDeleteBulkAction;
+use Filament\Actions\RestoreBulkAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\TrashedFilter;
+use Filament\Tables\Table;
+
+class RolesTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')->label('ID')->sortable(),
+                TextColumn::make('name')->label('Role Name')
+                ->searchable()
+                ->sortable(),
+                TextColumn::make('created_at')->label('Created At')->sortable()->searchable(),
+                TextColumn::make('updated_at')->label('Updated At')->sortable()->searchable()->toggleable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                DeleteAction::make(),
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                    ForceDeleteBulkAction::make(),
+                    RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+}
